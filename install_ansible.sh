ffb364c78e97062af074922527987e3d5ff213c2
Jonatas Baldin <jonatas.baldin@gmail.com>
2015-11-18 16:23:39 -0200

    add install_ansible.sh

---
+++ install_ansible.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to install Ansible from PPA or EPEL
+# If you want to improve it, just contribute!
+
+# Verify root
+if [ $(id -u) != 0 ] ; then
+    echo "You must be root!"
+    exit 1
+fi
+
+# Get OS
+# Ubuntu/CentOS
+if [ -f /etc/lsb-release ] ; then
+    . /etc/lsb-release
+    OS="$DISTRIB_ID"
+    VER="$DISTRIB_RELEASE"
+elif [ -f /etc/os-release ] ; then
+    . /etc/os-release
+    OS="$REDHAT_SUPPORT_PRODUCT" #centos 
+    VER="$REDHAT_SUPPORT_PRODUCT_VERSION" #7
+fi
+
+echo $OS
+echo $VER
+
+if [ "$OS" = "LinuxMint" -o "$OS" = "Ubuntu" ] ; then
+    apt-get install software-properties-common
+    apt-add-repository ppa:ansible/ansible -y
+    apt-get update -y 
+    apt-get install ansible -y 
+elif [ "$OS" = "centos" -a "$VER" -eq 7 ] ; then	
+    rpm -i https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum update -y
+    yum install ansible -y
+fi
